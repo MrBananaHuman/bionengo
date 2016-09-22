76f8406c1b94a6969c9737df691564d4ca5dbb8d
Peter Duggins <psipeter@gmail.com>
2016-09-22 13:41:48 -0400

    generated tuning curve

--- model.py
+++ model.py
@@ -15,15 +15,15 @@ import pandas as pd
 
 def make_spikes_in(P):
 	with nengo.Network() as model:
-		signal = nengo.Node(output=lambda x: np.sin(2*np.pi*x/P['t_sample']))
-		ens_in = nengo.Ensemble(5,dimensions=1)
-		probe_signal = nengo.Probe(signal)
+		signal = nengo.Node(output=lambda t: np.sin(2*np.pi*t/P['t_sample']))
+		ens_in = nengo.Ensemble(1,dimensions=1) 
+		probe_signal = nengo.Probe(signal) #get spikes from one neuron (one tuning curve)
 		probe_in = nengo.Probe(ens_in.neurons,'spikes')
 	with nengo.Simulator(model) as sim:
 		sim.run(P['t_sample'])
 		eval_points, activities = nengo.utils.ensemble.tuning_curves(ens_in,sim)
 	spikes_in=sim.data[probe_in]
-	return np.average(spikes_in,axis=1)/1000. #summed spike raster
+	return np.average(spikes_in,axis=1)/1000. #summed spike raster and neuron tuning curve
 	# return spikes_in #spike raster for all neurons
 
 def make_weights(P):
@@ -47,14 +47,14 @@ def make_bioneuron(P):
 
 def run(P,spikes_in,bioneuron):
 	neuron.init()
-	for t in np.arange(0,P['t_run']*1000,P['dt']):
+	for t in np.arange(0,P['t_sample']*1000,P['dt']):
 		if np.any(np.asfarray(np.where(spikes_in)[0]) == t):
 			for syn in bioneuron.synapses:
 				syn.conn.event(t)
 		neuron.run(t+P['dt'])
 
 def make_dataframe(P):
-	timesteps=np.arange(0,P['t_run'],P['dt'])
+	timesteps=np.arange(0,P['t_sample'],P['dt'])
 	columns=('time','voltage','spike','trial')
 	dataframe = pd.DataFrame(columns=columns, index=np.arange(0,len(timesteps)*P['n_trials']))
 	return dataframe
@@ -80,36 +80,40 @@ def plot_voltage(bioneuron):
 def get_tuning_curve(P,bioneuron,dataframe):
 	import seaborn as sns
 	import ipdb
-	timesteps=np.array(bioneuron.t_record)
+	timesteps=np.array(bioneuron.t_record)/1000.
 	h=np.exp(-timesteps/(P['tau_filter'])) #smooth spikes with exponential synaptic filter
-	h=h/np.sum(h)
+	# h=h/np.sum(h) #normalize??
 	avg_spikes=np.average([np.array(dataframe.query("trial==%s"%i)['spike'])
 							for i in range(P['n_trials'])],axis=0)
-	rates=np.array(np.convolve(avg_spikes,h,mode='full')[:len(avg_spikes)])
+	smoothed=np.array(np.convolve(avg_spikes,h,mode='full')[:len(avg_spikes)])
+	signal_in=np.array([np.sin(2*np.pi*t/P['t_sample']) for t in timesteps])
 
-	#construct Hz vs X
-	def signal_in(t):
-		return np.sin(2*np.pi*t/(P['t_sample']*1000)) #period 2pi/t_sample, t in msp
-
-	X=np.arange(np.min(signal_in(timesteps)),np.max(signal_in(timesteps)),P['dx'])
-	Hz=np.zeros_like(X)
-	for xi in range(len(X)-1):
+	X=np.arange(np.min(signal_in),np.max(signal_in),P['dx']) #eval points in X
+	Hz=np.zeros_like(X) #firing rate for each eval point
+	for xi in range(len(X)-1): #foreach eval point
 		ts=[]
+		#find the time indices where the signal is between this and the next evalpoint
 		for ti in range(len(timesteps)):
-			if X[xi] < signal_in(timesteps[ti]) < X[xi+1]:
+			if X[xi] < signal_in[ti] < X[xi+1]:
 				ts.append(ti)
-		Hz[xi]=np.average([rates[ti] for ti in ts])
+		#average the smoothed spike value at each of these time indices
+		Hz[xi]=np.average([smoothed[ti] for ti in ts])
+		#convert units to Hz by dividing by the time window
+		Hz[xi]=Hz[xi]/(timesteps[ts[-1]]-timesteps[ts[0]])
 
 	sns.set(context='poster')
-	figure1, ax1 = plt.subplots(1,1)
-	ax1.plot(timesteps,signal_in(timesteps),label='input signal')
-	ax1.plot(timesteps,rates,label='output rate')
-	ax1.set(xlabel='time (ms)', ylabel='firing rate (1/ms)')
+	figure1, (ax1,ax2) = plt.subplots(2,1)
+	ax1.plot(timesteps,signal_in,label='input signal')
+	ax1.plot(timesteps,avg_spikes,label='output spikes')
+	ax1.set(xlabel='time (s)', ylabel='firing rate (Hz)')
+	ax2.scatter(X,Hz,label='trials=%s'%P['n_trials'])
+	ax2.set(xlabel='x',ylabel='firing rate (Hz)')
 	plt.legend()
-	figure2, ax2 = plt.subplots(1,1)
-	ax2.scatter(X,Hz)
-	ax2.set(xlabel='x',ylabel='firing rate (1/ms)')
+	# figure3, ax3 = plt.subplots(1,1)
+	# ax3.plot(timesteps,h,label='filter')
+	# ax3.set(xlabel='time (s)', ylabel='filter value')
 	plt.show()
+	return X,Hz
 
 def main():
 	P=eval(open('parameters.txt').read())
@@ -123,7 +127,7 @@ def main():
 		run(P,spikes_in,bioneuron)
 		dataframe=update_dataframe(P,bioneuron,dataframe,i)
 		# plot_voltage(bioneuron)
-	get_tuning_curve(P,bioneuron,dataframe)
+	X,Hz=get_tuning_curve(P,bioneuron,dataframe)
 	# dataframe=make_dataframe(P,bioensemble)
 
 if __name__=='__main__':
