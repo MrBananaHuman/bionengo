w_max and tuning curve scaling fixes

analyze.py
@@ -40,22 +40,22 @@ def get_rates(P,spikes):
 	return spike_train, rates
 
 
-def make_tuning_curves(P,LIFdata,biorates):
+def make_tuning_curves(P,lifdata,biorates):
 	import numpy as np
 	import ipdb
 
-	X=np.arange(np.min(LIFdata['signal_in']),np.max(LIFdata['signal_in']),P['dx']) #eval points in X
+	X=np.arange(np.min(lifdata['signal_in']),np.max(lifdata['signal_in']),P['dx']) #eval points in X
 	Hz=np.zeros_like(X) #firing rate for each eval point
 	for xi in range(len(X)-1):
 		ts=[] #find the time indices where the signal is between this and the next evalpoint
 		for ti in range(len(np.arange(0,P['t_sample'],P['dt']))):
-			if X[xi] < LIFdata['signal_in'][ti] < X[xi+1]:
+			if X[xi] < lifdata['signal_in'][ti] < X[xi+1]:
 				ts.append(ti)
 		if len(ts)>0:
 			#average the firing rate at each of these time indices
 			Hz[xi]=np.average([biorates[ti] for ti in ts])
 			#convert units to Hz by dividing by the time window
-			Hz[xi]=Hz[xi]/len(ts)
+			# Hz[xi]=Hz[xi]/len(ts)
 	return X, Hz
 
 
@@ -91,7 +91,7 @@ def plot_rates(P,bioneuron,biospikes,biorates,raw_signal,lif_spikes,run_id):
 	ax1.plot(np.array(bioneuron.t_record)/1000, np.array(bioneuron.v_record))
 	ax1.set(xlabel='time', ylabel='bioneuron voltage (mV)')
 	ax2.plot(timesteps,raw_signal,label='input signal')
-	for n in range(P['n_LIF']):
+	for n in range(P['n_lif']):
 		ax2.plot(timesteps,np.array(lif_spikes)[:,n]*P['dt'],label='input spikes [%s]'%n)
 	ax2.plot(timesteps,biospikes*P['dt'],label='output spikes')
 	ax2.set(xlabel='time (s)')
@@ -113,14 +113,14 @@ def plot_tuning_curve(X,f_bio_rate,f_lif_rate,loss,run_id):
 	sns.set(context='poster')
 	figure, ax1 = plt.subplots(1,1)
 	ax1.plot(X,f_bio_rate(X),label='bioneuron firing rate (Hz)')
-	ax1.plot(X,f_lif_rate(X),label='LIF firing rate (Hz)')
+	ax1.plot(X,f_lif_rate(X),label='lif firing rate (Hz)')
 	ax1.set(xlabel='x',ylabel='firing rate (Hz)',title='loss=%0.3f' %loss)
 	plt.legend()
 	figure.savefig(run_id+'_tuning_curve.png')
 	plt.close(figure)
 
 
-def plot_loss(trials,hyp_params):
+def plot_loss(P,trials):
 	import matplotlib.pyplot as plt
 	import seaborn as sns
 	sns.set(context='poster')
@@ -129,13 +129,13 @@ def plot_loss(trials,hyp_params):
 	Y=[t['result']['loss'] for t in trials]
 	ax1.scatter(X,Y)
 	ax1.set(xlabel='$t$',ylabel='loss')
-	figure1.savefig(hyp_params['directory']+'hyperopt_result.png')
+	figure1.savefig(P['directory']+'hyperopt_result.png')
 
 
-def export_params(P,hyp_params,run_id):
+def export_params(P,run_id):
 	import pandas as pd
 	import json
-	my_params=pd.DataFrame([hyp_params])
+	my_params=pd.DataFrame([P])
 	my_params.reset_index().to_json(run_id+'_params.json',orient='records')
 
 def isi_hold_function(t, spike_times, midpoint=False, interp='zero'):
@@ -150,7 +150,6 @@ def isi_hold_function(t, spike_times, midpoint=False, interp='zero'):
 	    the points are placed at the beginning of ISIs
 	"""
 	isis = np.diff(spike_times)
-	print isis
 	if midpoint:
 		rt = np.zeros(len(isis)+2)
 		rt[0] = t[0]

bioneuron_response_curve.py
@@ -12,8 +12,8 @@ def main():
 	import json
 	import nengo
 	import pandas as pd
-	from initialize import ch_dir, make_addon, make_bioneuron
-	from run import run_neuron
+	from initialize import ch_dir, make_addon
+	from neuron_methods import run_bioneuron, make_bioneuron
 	import matplotlib.pyplot as plt
 	import seaborn as sns
 
@@ -21,9 +21,9 @@ def main():
 	P['timesteps']=np.arange(0,P['t_sample'],P['dt'])
 	datadir=ch_dir()
 	P['directory']=datadir
-	rates_in=np.arange(20,300,10)
+	rates_in=np.arange(20,200,10)
 	rates_out=np.zeros_like(rates_in)
-	w_naughts=np.logspace(-3,0,num=7)
+	w_naughts=np.logspace(-2,-1,num=5)
 	addon=make_addon(6)
 	P['n_LIF']=1
 
@@ -55,9 +55,9 @@ def main():
 			n_syn=P['synapses_per_connection']
 			weights=np.ones((P['n_LIF'],n_syn))*w0
 			locations=np.ones((P['n_LIF'],n_syn))*0.5 #synapses onto where spikes are recorded on soma
-			bias=0
+			bias=-1.0
 			bioneuron = make_bioneuron(P,weights,locations,bias)
-			run_neuron(P,LIFdata,bioneuron)
+			run_bioneuron(P,LIFdata,bioneuron)
 
 			timesteps=P['timesteps']
 			spike_times=np.round(np.array(bioneuron.spikes),decimals=3)

initialize.py
@@ -49,23 +49,23 @@ def make_signal(P):
 	assert raw_signal is not None, "signal type not specified"
 	return raw_signal
 
-def make_spikes_in(P,raw_signal,datadir):
+def make_spikes_in(P,raw_signal):
 	import nengo
 	import numpy as np
 	import pandas as pd
 	import json
 	spikes_in=[]
-	LIFdata={}
+	lifdata={}
 	while np.sum(spikes_in)==0: #rerun nengo spike generator until it returns something
 		with nengo.Network() as model:
 			signal = nengo.Node(
 					output=lambda t: raw_signal[int(t/P['dt'])])
 			ideal = nengo.Ensemble(1,
 					dimensions=1,
-					max_rates=nengo.dists.Uniform(P['min_LIF_rate'],P['max_LIF_rate']))
-			ens_in = nengo.Ensemble(P['n_LIF'],
+					max_rates=nengo.dists.Uniform(150,200)) #ideal tuning curve has limited rate
+			ens_in = nengo.Ensemble(P['n_lif'],
 					dimensions=1,
-					max_rates=nengo.dists.Uniform(P['min_LIF_rate'],P['max_LIF_rate']))		
+					max_rates=nengo.dists.Uniform(P['min_lif_rate'],P['max_lif_rate']))		
 			nengo.Connection(signal,ens_in)
 			probe_signal = nengo.Probe(signal)
 			probe_in = nengo.Probe(ens_in.neurons,'spikes')
@@ -74,19 +74,18 @@ def make_spikes_in(P,raw_signal,datadir):
 			eval_points, activities = nengo.utils.ensemble.tuning_curves(ideal,sim)
 		signal_in=sim.data[probe_signal]
 		spikes_in=sim.data[probe_in]
-	LIFdata['signal_in']=signal_in.ravel()
-	LIFdata['spikes_in']=spikes_in
-	LIFdata['lif_eval_points']=eval_points.ravel()
-	LIFdata['lif_activities']=activities.ravel()
-	out_data=pd.DataFrame([LIFdata])
-	out_data.reset_index().to_json(datadir+'LIFdata.json',orient='records')
-	return LIFdata
+	lifdata['signal_in']=signal_in.ravel()
+	lifdata['spikes_in']=spikes_in
+	lifdata['lif_eval_points']=eval_points.ravel()
+	lifdata['lif_activities']=activities.ravel()
+	out_data=pd.DataFrame([lifdata])
+	out_data.reset_index().to_json(P['directory']+'lifdata.json',orient='records')
+	return lifdata
 
-def find_w_max(P,LIFdata):
+def find_w_max(P,lifdata):
 	import numpy as np
 	from analyze import get_rates
-	summed_rates=get_rates(P,LIFdata['spikes_in'])
+	spike_train, summed_rates=get_rates(P,lifdata['spikes_in'])
 	rate_max=np.amax(summed_rates)
-	w_max=175.0/rate_max*0.01*200 #175 hz input from 0.01 max weight in response curve
-	print summed_rates,w_max
+	w_max=175.0/rate_max*0.01 #175 hz input from 0.01 max weight in response curve
 	return w_max

model.py
@@ -4,28 +4,13 @@ September 2016
 bionengo - interface NEURON and Bahr2 neurons with nengo
 '''
 
-def make_hyp_params(P,w_max,datadir):
-	import hyperopt
-	n_syn=P['synapses_per_connection']
-	n_LIF=P['n_LIF']
-	hyp_params={'P':P,'weights':{},'locations':{},'bias':0,'directory':datadir}
-	hyp_params['bias']=hyperopt.hp.uniform('b',P['bias_min'],P['bias_max'])
-	for n in range(n_LIF):
-		#adds a hyperopt-distributed weight, location, bias for each synapse
-		for i in range(n_syn): 
-			hyp_params['weights']['%s_%s'%(n,i)]=\
-					hyperopt.hp.uniform('w_%s_%s'%(n,i),-1.0*w_max,1.0*w_max)
-			if P['synapse_dist'] == 'optimized': 
-				hyp_params['locations']['%s_%s'%(n,i)]=\
-					hyperopt.hp.uniform('l_%s_%s'%(n,i),0,1)
-	return hyp_params
-
-def simulate(hyp_params):
+def simulate(P):
 	import numpy as np
 	import neuron
 	import hyperopt
 	import json
 	import ipdb
+	import os
 	from initialize import make_addon
 	from analyze import get_rates, make_tuning_curves, tuning_curve_loss,\
 						plot_rates, plot_tuning_curve,export_params
@@ -33,32 +18,30 @@ def simulate(hyp_params):
 	import timeit
 
 	start=timeit.default_timer()
-	P=hyp_params['P']
-	n_syn=P['synapses_per_connection']
-	n_LIF=P['n_LIF']
 	run_id=make_addon(6)
+	# if P['optimization']=='mongodb':
+	# 	os.chdir("home/pduggins/bionengo/data/"+P['directory'])
 
-	with open(hyp_params['directory']+"LIFdata.json") as data_file:  #linux  
-		LIFdata = json.load(data_file)[0]
-	weights=np.zeros((n_LIF,n_syn))
-	locations=np.zeros((n_LIF,n_syn))
-	bias=hyp_params['bias']
-	for n in range(n_LIF):
-		for i in range(n_syn):
-			weights[n][i]=hyp_params['weights']['%s_%s'%(n,i)]
-			if P['synapse_dist'] == 'optimized': 
-				locations[n][i]=hyp_params['locations']['%s_%s'%(n,i)]
+	with open(P['directory']+"lifdata.json") as data_file:  #linux  
+		lifdata = json.load(data_file)[0]
+	weights=np.zeros((P['n_lif'],P['n_syn']))
+	locations=np.zeros((P['n_lif'],P['n_syn']))
+	bias=P['bias']
+	for n in range(P['n_lif']):
+		for i in range(P['n_syn']):
+			weights[n][i]=P['weights']['%s_%s'%(n,i)]
+			locations[n][i]=P['locations']['%s_%s'%(n,i)]
 	bioneuron = make_bioneuron(P,weights,locations,bias)
 
 	print '\nRunning NEURON'
-	run_bioneuron(P,LIFdata,bioneuron)
+	run_bioneuron(P,lifdata,bioneuron)
 	spike_times=np.round(np.array(bioneuron.spikes),decimals=3)
 	biospikes, biorates=get_rates(P,spike_times)
-	bio_eval_points, bio_activities = make_tuning_curves(P,LIFdata,biorates)
-	plot_rates(P,bioneuron,biospikes,biorates,LIFdata['signal_in'],LIFdata['spikes_in'],run_id)
-	X,f_bio_rate,f_lif_rate,loss=tuning_curve_loss(P,LIFdata['lif_eval_points'],LIFdata['lif_activities'],bio_eval_points,bio_activities)
+	bio_eval_points, bio_activities = make_tuning_curves(P,lifdata,biorates)
+	plot_rates(P,bioneuron,biospikes,biorates,lifdata['signal_in'],lifdata['spikes_in'],run_id)
+	X,f_bio_rate,f_lif_rate,loss=tuning_curve_loss(P,lifdata['lif_eval_points'],lifdata['lif_activities'],bio_eval_points,bio_activities)
 	plot_tuning_curve(X,f_bio_rate,f_lif_rate,loss,run_id)
-	export_params(P,hyp_params,run_id)
+	export_params(P,run_id)
 	stop=timeit.default_timer()
 	print ' Neuron Runtime - %s sec' %(stop-start)
 	return {'loss': loss, 'status': hyperopt.STATUS_OK}
@@ -75,18 +58,27 @@ def main():
 
 	P=eval(open('parameters.txt').read())
 	datadir=ch_dir()
+	P['directory']=datadir
+	P['bias']=hyperopt.hp.uniform('b',P['bias_min'],P['bias_max'])
 	print 'Generating input spikes ...'
 	raw_signal=make_signal(P)
-	LIFdata=make_spikes_in(P,raw_signal,datadir)
-	w_max=find_w_max(P,LIFdata)
-	print 'wmax=', w_max
-	hyp_params=make_hyp_params(P,w_max,datadir)
-
+	lifdata=make_spikes_in(P,raw_signal)
+	w_max=find_w_max(P,lifdata)
+	# w_max=0.01
+	#adds a hyperopt-distributed weight, location, bias for each synapse
+	P['weights']={}
+	P['locations']={}
+	for n in range(P['n_lif']):
+		for i in range(P['n_syn']): 
+			P['weights']['%s_%s'%(n,i)]=hyperopt.hp.uniform('w_%s_%s'%(n,i),-1.0*w_max,1.0*w_max)
+			if P['synapse_dist'] == 'optimized': 
+				P['locations']['%s_%s'%(n,i)]=hyperopt.hp.uniform('l_%s_%s'%(n,i),0,1)	
+			elif P['synapse_dist'] == 'soma': 
+				P['locations']['%s_%s'%(n,i)]=0.5
 	if P['optimization']=='hyperopt':
 		trials=hyperopt.Trials()
-		best=hyperopt.fmin(simulate,space=hyp_params,algo=hyperopt.tpe.suggest,
-							max_evals=P['max_evals'],trials=trials)
-		plot_loss(trials,hyp_params)
+		best=hyperopt.fmin(simulate,space=P,algo=hyperopt.tpe.suggest,max_evals=P['max_evals'],trials=trials)
+		plot_loss(P,trials)
 
 	elif P['optimization']=='mongodb':
 		'''
@@ -99,10 +91,9 @@ def main():
 		TAB N: hyperopt-mongo-worker --mongo=localhost:1234/foo_db --poll-interval=0.1
 		'''
 		from hyperopt.mongoexp import MongoTrials
-		trials=MongoTrials('mongo://localhost:1234/foo_db/jobs', exp_key='exp3')
-		best=hyperopt.fmin(simulate,space=hyp_params,algo=hyperopt.tpe.suggest,
-							max_evals=P['max_evals'],trials=trials)
-		plot_loss(trials,hyp_params)
+		trials=MongoTrials('mongo://localhost:1234/foo_db/jobs', exp_key='exp1')
+		best=hyperopt.fmin(simulate,space=P,algo=hyperopt.tpe.suggest,max_evals=P['max_evals'],trials=trials)
+		plot_loss(P,trials)
 
 if __name__=='__main__':
 	main()

neuron_methods.py
@@ -1,21 +1,12 @@
-def make_bioneuron(P,weights,loc,bias):
+def make_bioneuron(P,weights,locations,bias):
 	import numpy as np
 	from neurons import Bahl
 	bioneuron=Bahl()
 	#make connections and populate with synapses
-	locations=None
-	n_syn=P['synapses_per_connection']
-	n_LIF=P['n_LIF']	
-	if P['synapse_dist'] == 'soma':
-		locations=np.ones(shape=(n_LIF,n_syn))*0.5
-	if P['synapse_dist'] == 'random':
-		locations=np.random.uniform(0,1,size=(n_LIF,n_syn))
-	elif P['synapse_dist'] == 'optimized':
-		locations=loc
-	for n in range(n_LIF):
+	for n in range(P['n_lif']):
 		bioneuron.add_bias(bias)
 		bioneuron.add_connection(n)
-		for i in range(n_syn):
+		for i in range(P['n_syn']):
 			syn_type=P['synapse_type']
 			if P['synapse_dist'] == 'soma': section=bioneuron.cell.soma(locations[n][i])
 			else: section=bioneuron.cell.apical(locations[n][i])
@@ -39,7 +30,7 @@ def run_bioneuron(P,LIFdata,bioneuron):
 		sys.stdout.flush()
 		t_idx=np.rint(t/P['dt'])
 		t_neuron=t*1000
-		for n in range(P['n_LIF']):
+		for n in range(P['n_lif']):
 			if np.any(np.where(np.array(LIFdata['spikes_in'])[:,n])[0] == t_idx):
 				for syn in bioneuron.connections[n]:
 					if syn.type=='ExpSyn': syn.conn.event(t_neuron)